b67f49f8bc123f0d9a176327950af018a947b096
guofangchao <guofangchao@daling.com>
2020-06-23 18:14:07 +0800

    修改文档

--- src/image_pro.rs
+++ src/image_pro.rs
@@ -5,7 +5,7 @@ use image::imageops::FilterType;
 use image::{GenericImage, ImageBuffer, ImageFormat};
 
 /**
- * 图片处理方法
+ * 图片处理方法,只需要压缩
  */
 pub fn image_fn(from_buf: &[u8]) -> Vec<u8> {
     let img = image::load_from_memory_with_format(from_buf, ImageFormat::Jpeg).unwrap();
@@ -14,7 +14,9 @@ pub fn image_fn(from_buf: &[u8]) -> Vec<u8> {
     buffer
 }
 
-//缩放图片
+/**
+ * 缩放图片
+ */
 fn zoom_image(from_buf: &[u8], width: u32, height: u32) -> Vec<u8> {
     let img = image::load_from_memory_with_format(from_buf, ImageFormat::Jpeg).unwrap();
 
